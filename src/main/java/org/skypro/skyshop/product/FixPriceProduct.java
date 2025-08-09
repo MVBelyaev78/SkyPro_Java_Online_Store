a738ba0684c35375dd375ecf965beb8d403d298c
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-10 01:55:17 +0300

    new class FixPriceProduct created

---
+++ src/main/java/org/skypro/skyshop/product/FixPriceProduct.java
@@ -0,0 +1,18 @@
+package org.skypro.skyshop.product;
+
+public class FixPriceProduct extends Product {
+    private static final Integer FIX_PRICE = 350;
+
+    public FixPriceProduct(String name) {
+        super(name);
+    }
+
+    public Integer getPrice() {
+        return FIX_PRICE;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %s", name, FIX_PRICE);
+    }
+}
