ead8c3db734b0a8f7e1b08bc9a3ea8e1a944a88f
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-10 16:10:50 +0300

    class Article added

---
+++ src/main/java/org/skypro/skyshop/articles/Article.java
@@ -0,0 +1,24 @@
+package org.skypro.skyshop.articles;
+
+public final class Article {
+    private final String title;
+    private final String text;
+
+    public Article(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s%n%s", title, text);
+    }
+}
